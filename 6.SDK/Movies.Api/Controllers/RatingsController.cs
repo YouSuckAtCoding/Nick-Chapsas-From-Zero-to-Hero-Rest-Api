27d8ae8b3b5d2121b53a96383c4684853f251aa0
Marco Antonio Fereira <marco.antonio.rg340@gmail.com>
2023-08-10 17:11:52 -0300

    Created a simple Sdk for the api using Refit.

---
+++ 6.SDK/Movies.Api/Controllers/RatingsController.cs
@@ -0,0 +1,57 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Auth;
+using Movies.Api.Mapping;
+using Movies.Application.Services;
+using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+[ApiVersion(1.0)]
+public class RatingsController : ControllerBase
+{
+    private readonly IRatingService _ratingService;
+
+    public RatingsController(IRatingService ratingService)
+    {
+        _ratingService = ratingService;
+    }
+
+    [Authorize]
+    [HttpPut(ApiEndpoints.Movies.Rate)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> RateMovie([FromRoute] Guid id,
+        [FromBody] RateMovieRequest request, CancellationToken token)
+    {
+        var userId = HttpContext.GetUserId();
+        var result = await _ratingService.RateMovieAsync(id, request.Rating, userId!.Value, token);
+        return result ? Ok() : NotFound();
+    }
+
+    [Authorize]
+    [HttpDelete(ApiEndpoints.Movies.DeleteRating)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteRating([FromRoute] Guid id,
+        CancellationToken token)
+    {
+        var userId = HttpContext.GetUserId();
+        var result = await _ratingService.DeleteRatingAsync(id, userId!.Value, token);
+        return result ? Ok() : NotFound();
+    }
+
+    [Authorize]
+    [HttpGet(ApiEndpoints.Ratings.GetUserRatings)]
+    [ProducesResponseType(typeof(IEnumerable<MovieRatingResponse>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetUserRatings(CancellationToken token = default)
+    {
+        var userId = HttpContext.GetUserId();
+        var ratings = await _ratingService.GetRatingsForUserAsync(userId!.Value, token);
+        var ratingsResponse = ratings.MapToResponse();
+        return Ok(ratingsResponse);
+    }
+}
---
+++ Movies.Api.Sdk.Consumer/AuthTokenProvider.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Api.Sdk.Consumer
+{
+    public class AuthTokenProvider
+    {
+        private readonly HttpClient _httpClient;
+        private string _cachedtoken = string.Empty;
+        private static readonly SemaphoreSlim Lock = new(1, 1);
+
+        public AuthTokenProvider(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetTokenAsync()
+        {
+            if(!string.IsNullOrEmpty(_cachedtoken))
+            {
+                var jwt = new JwtSecurityTokenHandler().ReadJwtToken(_cachedtoken);
+                var expireTimeText = jwt.Claims.Single(claim => claim.Type == "exp").Value;
+                var expiryDateTime = UnixTimeStampToDateTime(int.Parse(expireTimeText));
+                if(expiryDateTime > DateTime.UtcNow) 
+                {
+                    return _cachedtoken;
+                }
+
+            }
+
+            await Lock.WaitAsync();
+            var response = await _httpClient.PostAsJsonAsync("https://localhost:5003/token", new
+            {
+                userId = "d8566de3-b1a6-4a9b-b842-8e3887a82e42",
+                email = "seucu@nickchapsas.com",
+                customClaims = new Dictionary<string, object>
+                {
+                    {"admin", true },
+                    {"trusted_member", true }
+                }
+            });
+            var newToken = await response.Content.ReadAsStringAsync();
+            _cachedtoken = newToken;
+            Lock.Release();
+            return newToken;
+
+
+        }
+
+
+        private static DateTime UnixTimeStampToDateTime(int unixTimeStamp)
+        {
+            var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+
+    }
+}
